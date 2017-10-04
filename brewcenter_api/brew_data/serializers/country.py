2898b53e73e45e8e5900cc0ac6039ffaaaf0fe6f
Parth Verma <v.parth98@gmail.com>
2017-10-05 00:24:01 +0530

    removed comments and commented files

--- brewcenter_api/brew_data/serializers/country.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-# from brew_data.models import CountryCode
-#
-# class CountryCodeSerializer(serializers.ModelSerializer):
-#     """A simple serializer for fermentables"""
-#     class Meta:
-#         model = CountryCode
-#         fields = ('id','code')
