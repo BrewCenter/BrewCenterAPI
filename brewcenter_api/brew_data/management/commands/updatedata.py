c9a4ee60f254176ea55115d13c053e37ad4a9d02
Brian Painter <brian.m.painter@gmail.com>
2017-10-28 12:12:37 -0400

    updating the management command to be more pythonic

--- brewcenter_api/brew_data/management/commands/updatedata.py
+++ brewcenter_api/brew_data/management/commands/updatedata.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 from brew_data.models import CountryCode, FermentableType, Fermentable, FermentableInstance, HopType, Hop, YeastType, Yeast, Style
 import brew_data.data_miner.brew_target.miner as brew_target_miner
 
+
 class Command(BaseCommand):
     def handle(self, **options):
         brew_target_miner.mine(self.stdout)
@@ -17,29 +18,24 @@ class Command(BaseCommand):
 
         # save all country codes used
         s.execute("SELECT code from countrycode;")
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             CountryCode.objects.get_or_create(code=cur[0])
             n += 1
-            cur = s.fetchone()
-        self.stdout.write("Updated db with {0} CountryCodes".format(n))
+        self.stdout.write('Updated db with {0} CountryCodes'.format(n))
 
         # save all fermentable types used
         s.execute("SELECT name from fermentabletype;")
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             FermentableType.objects.get_or_create(name=cur[0])
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} FermentableTypes".format(n))
 
         # save all fermentables found
         s.execute("SELECT {0} from fermentable;".format(brew_target_miner.Fermentable.get_keys()))
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             # check for collisions
             fermentable = Fermentable.objects.get_or_create(
                 name=cur[0],
@@ -59,24 +55,20 @@ class Command(BaseCommand):
                 is_active=True)
                 
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} Fermentables".format(n))
 
         # save all hop types used
         s.execute("SELECT name from hoptype;")
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             HopType.objects.get_or_create(name=cur[0])
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} HopTypes".format(n))
 
         # save all hops found
         s.execute("SELECT {0} from hop;".format(brew_target_miner.Hop.get_keys()))
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             # check for collisions
             if Hop.objects.filter(name=cur[0]).count() == 0:
                 Hop.objects.create(
@@ -89,24 +81,20 @@ class Command(BaseCommand):
                 )
 
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} Hops".format(n))
 
         # save all yeast types used
         s.execute("SELECT name from yeasttype;")
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             YeastType.objects.get_or_create(name=cur[0])
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} YeastTypes".format(n))
 
         # save all yeast found
         s.execute("SELECT {0} from yeast;".format(brew_target_miner.Yeast.get_keys()))
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             # check for collisions
             if Yeast.objects.filter(name=cur[0]).count() == 0:
                 Yeast.objects.create(
@@ -122,14 +110,12 @@ class Command(BaseCommand):
                 )
 
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} Yeast".format(n))
 
         # save all styles found
         s.execute("SELECT {0} from styles;".format(brew_target_miner.Style.get_keys()))
-        cur = s.fetchone()
         n = 0
-        while cur:
+        for cur in s.fetchall():
             # check for collisions
             if Style.objects.filter(name=cur[0]).count() == 0:
                 Style.objects.create(
@@ -149,8 +135,6 @@ class Command(BaseCommand):
                 )
 
             n += 1
-            cur = s.fetchone()
         self.stdout.write("Updated db with {0} Styles".format(n))
-
 
         source.close()
