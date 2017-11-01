de3c5dd2d41c572e8847d21b0a4c25101adcacc6
Michael Washburn Jr <mdw7326@rit.edu>
2017-11-01 19:17:47 -0400

    Add working fermentable suggestion models

--- brewcenter_api/brew_data/views/__init__.py
+++ brewcenter_api/brew_data/views/__init__.py
@@ -1,7 +1,7 @@
 from brew_data.views.all import All
 from brew_data.views.styles import Styles
 from brew_data.views.countries import Countries
-from brew_data.views.fermentables import FermentableTypes, Fermentables
+from brew_data.views.fermentables import FermentableTypes, FermentableInstances, Fermentables
 from brew_data.views.hops import HopTypes, Hops
 from brew_data.views.yeast import YeastTypes, Yeast
 from brew_data.views.suggestions import Suggestions
