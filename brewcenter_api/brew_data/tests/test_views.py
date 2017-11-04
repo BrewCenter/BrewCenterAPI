4ef6337a4c83b89d65fafb7a0cc89fb4fdc7e67f
Annaliese Pietsch <apietsch@vailsys.com>
2017-11-04 18:28:32 -0500

    add hops view tests

--- brewcenter_api/brew_data/tests/test_views.py
+++ brewcenter_api/brew_data/tests/test_views.py
@@ -13,3 +13,21 @@ class YeastTestCase(TestCase):
     def test_yeast_types_get(self):
         response = self.client.get('/data/ingredients/yeast/types')
         assert response.status_code == 200
+
+class HopTestCase(TestCase):
+    """Test for Hops and HopTypes View"""
+    def setUp(self):
+        self.client = APIClient()
+        
+    def tearDown(self):
+        del(self.client)
+    
+    def test_hops_get(self):
+        response = self.client.get('/data/ingredients/hops')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_hop_types_get(self):
+        response = self.client.get('/data/ingredients/hops/types')
+        self.assertEqual(response.status_code, 200)
+    
+    
