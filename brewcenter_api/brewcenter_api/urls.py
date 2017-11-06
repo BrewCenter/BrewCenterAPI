2905ce6e36cd1a128ea0ba99c1457af0d6b37330
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2017-11-05 23:05:07 -0500

    Removed some other linter errors

--- brewcenter_api/brewcenter_api/urls.py
+++ brewcenter_api/brewcenter_api/urls.py
@@ -1,4 +1,5 @@
-"""brewcenter_api URL Configuration
+"""
+brewcenter_api URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
